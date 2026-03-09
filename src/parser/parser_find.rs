2cc5473f96ddeab8b9bb1ab1e56403de8297485b
Artur Kreihs <akreihs@ascs.pl>
2026-03-09 21:21:13 +0100

    ^ Pinger pings with c cmd;

--- src/parser/parser_find.rs
+++ src/parser/parser_find.rs
@@ -1,8 +1,15 @@
+use std::time::Duration;
+
 use crate::net::Net;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use pinger::Pinger;
+use owo_colors::OwoColorize;
 
 #[ipchanger_macros::command(key = 'c', name = "check", usage = "c<ip>", description = "Check IPv4 address")]
-pub fn check(_: &Net, _: Option<&str>) -> Result<()> {
-    // println!("checking");
+pub fn check(_: &Net, addr: Option<&str>) -> Result<()> {
+    let p = Pinger::new()?
+        .set_timeout(Duration::from_secs(1))?;
+    p.ping(addr.ok_or(anyhow!("invalid address"))?.parse()?)?;
+    println!("{}", "Success".green());
     Ok(())
 }
